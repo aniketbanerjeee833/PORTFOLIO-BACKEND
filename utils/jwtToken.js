4a3f978bdc74affcd03cc9e4a7bde03153fdee75
Aniket Banerjee <aniketbanerjee8334845906@gmail.com>
2024-08-15 01:29:27 +0530

    commited

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,7 @@ const token = user.createJwt();
 
 
   res.status(statusCode)
-  .cookie("token", token, {expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),httpOnly: true,sameSite:none,secure:true})
+  .cookie("token", token, {expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),httpOnly: true,sameSite:"None",secure:true})
   .json({success: true,message,user,token})
 }
 
